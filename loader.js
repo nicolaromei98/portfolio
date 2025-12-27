ac0333c76e937775d98f4124ec2bf8173dcc285c
nicolaromei98 <167851712+nicolaromei98@users.noreply.github.com>
2025-12-27 08:41:20 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -26,7 +26,7 @@
       ease: "sine.inOut"
     },
     reveal: {
-      lines: { duration: 0.8, stagger: 0.08 },
+      lines: { duration: 1, stagger: 0.08 },
       words: { duration: 0.6, stagger: 0.06 },
       chars: { duration: 0.4, stagger: 0.01 }
     }
